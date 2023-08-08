8bfd9e8baeab7ad817b3e310889526d4aedead86
Andrew Keinsley <andrewkeinsley@weber.edu>
2023-08-08 09:33:59 -0600

    Matched the necessary bills and notes.

--- Analysis/Amihud_Mendelson/AM91_Expansion.py
+++ Analysis/Amihud_Mendelson/AM91_Expansion.py
@@ -20,7 +20,7 @@ df = df.dropna()
 # Dropping negative values from the numeric columns
 numvals = df.select_dtypes(include=['float64'])
 posvals = (numvals[numvals.columns] >= 0).all(axis=1)
-df = df[posvals].reset_index()
+df = df[posvals].reset_index(drop=True)
 
 # Checking the data for anymore issues
 df.info()
@@ -40,3 +40,25 @@ df['m2mat'] = df['m2mat'].astype(int)
 df['d2mat'] = ((df.tmatdt - df.mcaldt)/np.timedelta64(1, 'D'))
 df['d2mat'] = df['d2mat'].astype(int)
 
+##### SEPARATING BY TYPE #####
+
+bills = df.loc[df['itype'] == 4]
+bills['d2mat_check'] = bills['d2mat']
+notes = df.loc[df['itype'] == 2]
+bonds = df.loc[df['itype'] == 1]
+
+notes = notes.sort_values(['d2mat'], ascending=[True])
+bills = bills.sort_values(['d2mat'], ascending=[True])
+
+##### MATCHING BILLS TO NOTES #####
+notes = pd.merge_asof(notes, bills, on='d2mat', by='mcaldt', direction='nearest', tolerance=3,suffixes=('_Note', '_Bill'))
+
+notes = notes.sort_values(['mcaldt','d2mat'], ascending=[True,True])
+
+notes_matched = notes.dropna()
+
+##### CALCULATING RELATIVE RATES #####
+
+notes_matched['tmpcyld_Spread'] = notes_matched['tmpcyld_Note'] - notes_matched['tmpcyld_Bill']
+notes_matched['baspread_Spread'] = notes_matched['baspread_Note'] - notes_matched['baspread_Bill']
+notes_matched['tcouprt_Spread'] = notes_matched['tcouprt_Note'] - notes_matched['tcouprt_Bill']
